6f87e0edb436072f6e91e7a7dd91627926726f04
unscriptable <john@unscriptable.com>
2013-12-31 09:45:50 -0500

    More work on pipeline extensions.
    * Implement all default-level loader pipeline extensions in case loader shim does not implement them the way we expect.
    * Implement node pipeline.

---
+++ pipeline/node.js
@@ -0,0 +1,18 @@
+/** @license MIT License (c) copyright 2014 original authors */
+/** @author Brian Cavalier */
+/** @author John Hann */
+var normalizeCjs = require('./normalizeCjs');
+var locateNpm = require('./locateNpm');
+var fetchAsText = require('./fetchAsText');
+var translateAsIs = require('./translateAsIs');
+var instantiateNode = require('./instantiateNode');
+
+module.exports = function () {
+	return {
+		normalize: normalizeCjs,
+		locate: locateNpm,
+		fetch: fetchAsText,
+		translate: translateAsIs,
+		instantiate: instantiateNode
+	};
+};
--- pipeline/translateAsIs.js
+++ pipeline/translateAsIs.js
@@ -1,8 +1,8 @@
 /** @license MIT License (c) copyright 2014 original authors */
 /** @author Brian Cavalier */
 /** @author John Hann */
-module.exports = translateNoop;
+module.exports = translateAsIs;
 
-function translateNoop (load) {
+function translateAsIs (load) {
 	return load.source;
 }
