aba72e328e645f7a4c80d0ff0a659b1557e15e7c
unscriptable <john@unscriptable.com>
2014-01-30 20:04:02 -0500

    Display more fun stuff.

--- experiments/displayMetadata.js
+++ experiments/displayMetadata.js
@@ -1,7 +1,9 @@
 module.exports = displayMetadata;
 
 function displayMetadata (context) {
-	write('done! Found the following packages:');
+	write('This module\'s uri is "' + module.uri + '".');
+	write('This module\'s id is "' + module.id + '".');
+	write('Found the following packages:');
 	write(JSON.stringify(Object.keys(context.packages), null, '    '), 'pre');
 	console.log(context);
 }
