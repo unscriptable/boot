cca7fdbbf7fc99b616f9ec194366b8300f8cb577
unscriptable <john@unscriptable.com>
2014-01-31 09:53:04 -0500

    Move formatting out to a different module to test if the loader pipeline really works.

--- experiments/displayMetadata.js
+++ experiments/displayMetadata.js
@@ -1,10 +1,12 @@
+var formatPackages = require('./formatPackages');
+
 module.exports = displayMetadata;
 
 function displayMetadata (context) {
 	write('This module\'s uri is "' + module.uri + '".');
 	write('This module\'s id is "' + module.id + '".');
 	write('Found the following packages:');
-	write(JSON.stringify(Object.keys(context.packages), null, '    '), 'pre');
+	write(formatPackages(context.packages), 'pre');
 	console.log(context);
 }
 
