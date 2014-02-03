4dc7065fbaaff3fec2810ff4d1a12c2539780bab
unscriptable <john@unscriptable.com>
2014-02-03 16:21:49 -0500

    Update experiment to pull in two modules (AMD and UMD) via bower and a UMD module via npm.

--- experiments/displayMetadata.js
+++ experiments/displayMetadata.js
@@ -1,15 +1,23 @@
 var formatPackages = require('./formatPackages');
 
+var when = require('when');
+var domReady = require('curl/src/curl/domReady');
+var rest = require('rest');
+
 module.exports = displayMetadata;
 
 function displayMetadata (context) {
 	write('This module\'s uri is "' + module.uri + '".');
 	write('This module\'s id is "' + module.id + '".');
-	write('Found the following packages:');
+	write('Configured the following packages:');
 	write(formatPackages(context.packages), 'pre');
 	console.log(context);
 }
 
 function write (msg, tagType) {
-	document.body.appendChild(document.createElement(tagType || 'p')).innerHTML = msg;
+	domReady(function () {
+		var doc = document;
+		var body = doc.body;
+		body.appendChild(doc.createElement(tagType || 'p')).innerHTML = msg;
+	});
 }
